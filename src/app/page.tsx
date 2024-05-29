3cc29ccefc1319d29b827c0f1bdac56d49a31283
Dima-dev-front <freelance.dudolad.d@gmail.com>
2024-05-29 15:20:57 +0300

    Update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,27 @@
+import Link from 'next/link';
+
 const HomePage = () => (
   <div
-    className='flex h-screen items-center justify-center
+    className='flex h-screen flex-col items-center justify-center
         bg-gradient-to-r from-slate-900 to-slate-700 p-3'
   >
-    <div className='animate-slideDown flex flex-col items-center gap-2 opacity-0 lg:flex-row'>
-      <div
-        className='max-w-fit text-wrap bg-gradient-to-r from-cyan-500
-          to-fuchsia-600 bg-clip-text text-center text-3xl font-semibold text-transparent lg:text-6xl'
-      >
+    <div
+      className='animate-slideDown mb-4 flex flex-col
+        items-center gap-2 opacity-0 lg:flex-row'
+    >
+      <div className='max-w-fit text-wrap bg-gradient-to-r from-fuchsia-100 to-cyan-300 bg-clip-text text-center text-3xl font-semibold text-transparent lg:text-6xl'>
         Welcome to the Next.js demo app
         <span className='h-fit text-4xl text-[initial] lg:text-6xl'>👋</span>
       </div>
     </div>
+    <Link
+      href='/friends'
+      className='animate-[slideDown_0.5s_ease-out_forwards_0.8s] bg-gradient-to-r
+        from-fuchsia-200 to-cyan-400 bg-clip-text text-2xl font-semibold text-transparent
+        underline decoration-1 opacity-0'
+    >
+      Go to Friends page
+    </Link>
   </div>
 );
 
