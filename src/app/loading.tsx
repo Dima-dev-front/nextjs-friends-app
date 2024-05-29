9fcb3308f90e241e975cfd0fc593cb15abc42472
Dima-dev-front <freelance.dudolad.d@gmail.com>
2024-05-29 19:15:09 +0300

    Add README, and "back link" to "Friends" page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,9 @@
 const Loading = () => (
-  <div className='min-w-screen flex flex-1 items-center justify-center bg-gray-100 p-5'>
-    <div className='flex space-x-2'>
-      <div className='h-5 w-5 animate-bounce rounded-full bg-white [animation-delay:-0.3s]'></div>
-      <div className='h-5 w-5 animate-bounce rounded-full bg-white [animation-delay:-0.15s]'></div>
-      <div className='h-5 w-5 animate-bounce rounded-full bg-white'></div>
+  <div className='min-w-screen flex min-h-screen flex-1 items-center justify-center bg-gray-100 p-5'>
+    <div className='mx-auto flex space-x-2'>
+      <div className='h-5 w-5 animate-bounce rounded-full bg-cyan-500 [animation-delay:-0.3s]'></div>
+      <div className='h-5 w-5 animate-bounce rounded-full bg-cyan-500 [animation-delay:-0.15s]'></div>
+      <div className='h-5 w-5 animate-bounce rounded-full bg-cyan-500'></div>
     </div>
   </div>
 );
